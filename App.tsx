5ca6fe9b57183ce55eaedf537e7585b435aeb0f9
kevin cuadros <kevindavidcuadros@gmail.com>
2024-04-22 20:36:07 -0500

    Corregir importaciones y agregar pantalla BoxObjectModelScreen en App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 import { PaperProvider } from 'react-native-paper'
+import { BoxObjectModelScreen } from './src/presentation/screens/BoxObjectModelScreen'
+import IonIcon from 'react-native-vector-icons/Ionicons'
 import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen'
 import { CounterScreen } from './src/presentation/screens/CounterScreen'
 import { HelloScreen } from './src/presentation/screens/HelloScreen'
-import IonIcon from 'react-native-vector-icons/Ionicons'
 
 export const App = () => {
   return (
@@ -16,7 +17,8 @@ export const App = () => {
       <SafeAreaView style={{ flex: 1 }}>
         {/* <HelloScreen name='Kevin Cuadros'/> */}
         {/* <CounterScreen /> */}
-        <CounterM3Screen />
+        {/* <CounterM3Screen /> */}
+        <BoxObjectModelScreen />
       </SafeAreaView>
     </PaperProvider>
   )
---
+++ src/presentation/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const BoxObjectModelScreen = () => {
+  return (
+    <View style={styles.container}>
+      {/* <Text style={styles.title}>BoxObjectModel</Text> */}
+      <View style={styles.purpleBox}></View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    flex: 1,
+  },
+  title: {
+    fontSize: 30,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderWidth: 10,
+  },
+  purpleBox: {
+    height: 30,
+    backgroundColor: 'purple',
+    margin: 20,
+  }
+})
